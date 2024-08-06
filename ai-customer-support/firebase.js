87972c3e050fd59697a9375057f94def10f6bc02
jyanokye <jamesyawanokye17@gmail.com>
2024-08-06 15:21:15 +0000

    Pantry

---
+++ ai-customer-support/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDM9x8iH_3j9W-RrUbGJ5VlEKci-TTvFIQ",
+  authDomain: "ai-customer-support-cbef7.firebaseapp.com",
+  projectId: "ai-customer-support-cbef7",
+  storageBucket: "ai-customer-support-cbef7.appspot.com",
+  messagingSenderId: "144694712460",
+  appId: "1:144694712460:web:4b7d5ceaab9127bfd743a9",
+  measurementId: "G-RHK6J979H8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
