5bca739691e2bc48cd5d0baef248043bcb2f2d3e
jyanokye <jamesyawanokye17@gmail.com>
2024-08-10 04:26:05 +0000

    JAMES

--- ai-customer-support/firebase.js
+++ ai-customer-support/firebase.js
@@ -1,20 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDM9x8iH_3j9W-RrUbGJ5VlEKci-TTvFIQ",
-  authDomain: "ai-customer-support-cbef7.firebaseapp.com",
-  projectId: "ai-customer-support-cbef7",
-  storageBucket: "ai-customer-support-cbef7.appspot.com",
-  messagingSenderId: "144694712460",
-  appId: "1:144694712460:web:4b7d5ceaab9127bfd743a9",
-  measurementId: "G-RHK6J979H8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
